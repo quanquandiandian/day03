1715dc0a14bff78d6fdc7905da575e427c998f79
周三 <123@153.com>
2020-09-23 20:53:55 +0800

    完结

--- classmanager/classs/apps.py
+++ classmanager/classs/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class ClassConfig(AppConfig):
     name = 'classs'
+    verbose_name ='球队'
--- classmanager/classs/views.py
+++ classmanager/classs/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-
-    return HttpResponse('ok')
+    context = {
+        'name':'ni'
+    }
+    return render(request,'index.html',context=context)
